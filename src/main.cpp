f3f700669a3120fb5e4ce7d63b6ad8ecccfc13a3
Scott Loughmiller <loughmsa@gmail.com>
2020-01-07 18:10:44 -0500

    initial commit, simple lights working

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+#include <FastLED.h>
+#include <Visualization.h>
+
+// FAST LED
+#define NUM_LEDS 288
+#define DISPLAY_LED_PIN 2
+
+CRGB leds[NUM_LEDS];
+
+#define SATURATION 244
+
+Visualization * all;
+
+
+void setup() {
+  delay(1000);
+
+  Serial.begin(9600);
+  Serial.println("setup started");
+
+  // DISPLAY STUFF
+  FastLED.addLeds<NEOPIXEL, DISPLAY_LED_PIN>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );;
+  all = new Visualization(NUM_LEDS, 1, 0, 0, leds);
+
+  //FastLED.setBrightness(32);
+  all->setAllCRGB(0x003000);
+  FastLED.show();
+  delay(1000);
+
+  Serial.println("setup complete");
+}
+
+void loop() {
+  all->setAllCRGB(0x000000);  // this only clears the array, not the LEDs, it's fine at the top
+
+  all->setAllCRGB(0xFF0004);
+
+  FastLED.show();
+}
